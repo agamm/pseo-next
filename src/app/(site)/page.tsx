4395cc2cf14a7b8f0e1052f5bb038754aa36b836
Agam More <agam@agam.me>
2023-03-16 18:14:05 +0700

    Minor non relevant changes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,9 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
   const myIP = await getIP();
+  const url = "/test";
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello {myIP} !</h1>
+      <p>I like to walk to {url}</p>
       <ExampleComponent />
     </main>
   );
